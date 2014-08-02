dac2115012f0a23f0e7b856ac18c7a51c22d33d8
Blake Regalia <blake.regalia@gmail.com>
2014-08-02 10:37:03 -0700

    all all

--- app/server.js
+++ app/server.js
@@ -744,12 +744,12 @@ app.get('/captured', function(req, res, next) {
 			var s_movie = a_movies[i];
 			console.log(s_movie.substr(0, s_movie.length-4));
 			fs.readFile(CAPTURE_DIR+'/'+s_movie.substr(0, s_movie.length-4)+'.duration', {encoding:'utf8'}, function(err, data) {
-				console.log(err, data);
 				if(!err) {
 					h_durations[s_movie] = data;
 				}
 			});
 		}
+		console.log(h_durations);
 		res.send(h_durations);
 	});
 
